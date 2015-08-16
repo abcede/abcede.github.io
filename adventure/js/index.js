ec60ce764130348d1bd441851eb21c4485ba71a8
abcede <tullisgt1@hotmail.com>
2015-08-16 20:59:40 +0800

    test fix fix

--- adventure/js/index.js
+++ adventure/js/index.js
@@ -1,5 +1,5 @@
 //button
-function startGame()
+//function startGame()
 //load bg image before everything else
 $("body").css("background-image: url(http://cdn.playbuzz.com/cdn/09f8f7dd-c046-4a1f-8339-f40c0ddc786f/49887369-2992-45c9-9eb9-7a4f526c6fb7.jpg) ;")
 
